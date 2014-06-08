3f99e161938bb33f1f5e3687c875f54541aaff7f
SevereOverfl0w <overfl0w@narna.co>
2014-06-08 18:55:33 +0100

    Some frontend stuff.

--- krympa/krympa/static/app.js
+++ krympa/krympa/static/app.js
@@ -1,14 +1,17 @@
 $('#shortenForm').submit(function(e){
-    postData = $(this).serializeArray();
-    var formURL = $(this).attr('action');
-
+    e.preventDefault();
+    var form = $(this);
+    postData = form.serializeArray();
+    var formURL = form.attr('action');
     $.ajax({
         url: formURL,
         type: 'POST',
         data: postData,
         success: function(data, textStatus, jqXHR){
+            form.children('.errors').remove();
             if ( data.status === 'error' ){
-                var errordiv = '<div class="errors"><h2 class="form-error-title">Oops. The following error(s) occurred.</h2><ul>';
+                var word = Object.keys(data.response.errmsg).length == 1 ? 'error' : 'errors';
+                var errordiv = '<div class="errors"><h2 class="form-error-title">Oops. The following ' + word + ' occurred.</h2><ul>';
                 for (var key in data.response.errmsg){
                     errordiv += '<li class="form-error">' + key + ': ' + data.response.errmsg[key] + '</li>';
                 }
@@ -17,12 +20,14 @@ $('#shortenForm').submit(function(e){
                     errordiv
                     );
             } else if ( data.status === 'success' ){
-                $('#urlInput').val( data.response.short )
+                form.fadeOut(function(){
+                    $('#urlInput').val( data.response.short );
+                    form.fadeIn();
+                });
             }
         },
         error: function(jqXHR, textStatus, errorThrown){
             console.log(textStatus)
         }
     });
-    e.preventDefault();
 });
--- krympa/krympa/templates/index.mako
+++ krympa/krympa/templates/index.mako
@@ -2,19 +2,20 @@
 <html>
 <head>
     <title>${project}</title>
+    <link href="${request.static_url('krympa:static/style.css')}" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <header id="header">
-        <h1 id="title">${project} URL Shortener</h1>
-        <img src="${request.static_url('krympa:static/logo.png')}" />
-    </header>
-    <section id="shortener">
-        <form action="${request.route_url('api')}" method="POST" id="shortenForm">
-            <label id="urlLabel" for="url">URL</label>
-            <input type="text" name="url" id="urlInput" />
-            <button type="submit" id="shortenBtn">Submit</button>
-        </form>
-    </section>
+    <main class="main">
+        <header id="header">
+            <h1 id="title">${project} URL Shortener</h1>
+        </header>
+        <section id="shortener">
+            <form action="${request.route_url('api')}" method="POST" id="shortenForm">
+                <input type="text" name="url" id="urlInput" placeholder="Enter link to shorten it." />
+                <button type="submit" id="shortenBtn">Shorten</button>
+            </form>
+        </section>
+    </main>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="${request.static_url('krympa:static/app.js')}"></script>
 </body>
